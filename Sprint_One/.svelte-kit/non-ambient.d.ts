596b0d75176b656f6c14639387bac22866bebde0
Conor Foley <c00301460@setu.ie>
2026-09-14 12:18:27 +0100

    Added files needed for sprint2, fixed bug in sprint1 work where one of the tests was expecting the wrong value

--- Sprint_One/.svelte-kit/non-ambient.d.ts
+++ Sprint_One/.svelte-kit/non-ambient.d.ts
@@ -29,14 +29,15 @@ declare module "$app/types" {
 	type MatcherParam<M> = M extends (param : string) => param is (infer U extends string) ? U : string;
 
 	export interface AppTypes {
-		RouteId(): "/";
+		RouteId(): "/" | "/combined";
 		RouteParams(): {
 			
 		};
 		LayoutParams(): {
-			"/": Record<string, never>
+			"/": Record<string, never>;
+			"/combined": Record<string, never>
 		};
-		Pathname(): "/";
+		Pathname(): "/" | "/combined";
 		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
 		Asset(): "/robots.txt" | string & {};
 	}
--- Sprint_One/src/test/biggestWithSameDigits.test.ts
+++ Sprint_One/src/test/biggestWithSameDigits.test.ts
@@ -4,7 +4,7 @@ import {biggest} from '../lib/filters';
 describe('Biggest with same digit filter', () => {
     it ('Takes an integer sequence and returns the largest number that can be made with the individual digits of each number in the sequence', () => {
         const input = [12, 143, 471, 1010, 5678];
-        const expected = [21, 431, 741, 1100, 7865];
+        const expected = [21, 431, 741, 1100, 8765];
         expect(biggest(input)).toEqual(expected);
     });
 });
